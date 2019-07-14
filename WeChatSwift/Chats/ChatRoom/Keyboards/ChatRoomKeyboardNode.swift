6863efddcc13da730957bb965deb90da708fd7fb
alexiscn <xshf87@126.com>
2019-07-14 22:31:11 +0800

    Edit Keyboard

--- WeChatSwift/Chats/ChatRoom/Keyboards/ChatRoomKeyboardNode.swift
+++ WeChatSwift/Chats/ChatRoom/Keyboards/ChatRoomKeyboardNode.swift
@@ -22,9 +22,12 @@ class ChatRoomKeyboardNode: ASDisplayNode {
     
     private let panelHeight: CGFloat
     
+    private let bottomInset: CGFloat
+    
     init(barHeight: CGFloat = 60.0, panelHeight: CGFloat = 216.0) {
         self.barHeight = barHeight
         self.panelHeight = panelHeight
+        self.bottomInset = Constants.bottomInset
         
         super.init()
         
@@ -38,9 +41,9 @@ class ChatRoomKeyboardNode: ASDisplayNode {
     override func didLoad() {
         super.didLoad()
         
-//        let toolBarHeight: CGFloat = 60.0
-//        let toolBarY = Constants.screenHeight - Constants.topInset - Constants.bottomInset - 44.0 - toolBarHeight
-//        let toolBarFrame = CGRect(x: 0, y: toolBarY, width: Constants.screenWidth, height: toolBarHeight)
+        let offsetY = Constants.screenHeight - Constants.topInset - Constants.bottomInset - 44.0 - barHeight
+        let height = barHeight + bottomInset + panelHeight
+        self.frame = CGRect(x: 0, y: offsetY, width: Constants.screenWidth, height: height)
         
         backgroundColor = UIColor(hexString: "#F5F6F7")
         
@@ -57,12 +60,19 @@ class ChatRoomKeyboardNode: ASDisplayNode {
                 self.toolsPanel.isHidden = true
                 self.emotionPanel.isHidden = false
                 self.lastKeyboardOffsetY = self.frame.origin.y
-                
-                self.frame = CGRect(x: 0, y: self.supernode!.frame.height - self.bounds.height, width: Constants.screenWidth, height: self.bounds.height)
-                
+                self.frame.origin = CGPoint(x: 0, y: self.supernode!.frame.height - self.bounds.height)
+                self.emotionPanel.frame.origin = CGPoint(x: 0, y: self.barHeight + self.bottomInset)
+                self.toolsPanel.frame.origin = CGPoint(x: 0, y: self.bounds.height)
             }, completion: nil)
         case .tools:
-            print("....")
+            UIView.animate(withDuration: 0.25, delay: 0, options: .curveLinear, animations: {
+                self.toolsPanel.isHidden = false
+                self.emotionPanel.isHidden = true
+                self.lastKeyboardOffsetY = self.frame.origin.y
+                self.frame.origin = CGPoint(x: 0, y: self.supernode!.frame.height - self.bounds.height)
+                self.emotionPanel.frame.origin = CGPoint(x: 0, y: self.bounds.height)
+                self.toolsPanel.frame.origin = CGPoint(x: 0, y: self.barHeight + self.bottomInset)
+            }, completion: nil)
         case .none:
             guard let beginFrame = notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? CGRect,
                 let endFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
@@ -86,6 +96,18 @@ class ChatRoomKeyboardNode: ASDisplayNode {
     }
     
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        return ASInsetLayoutSpec(insets: .zero, child: toolBar)
+        
+        toolBar.style.preferredSize = CGSize(width: Constants.screenWidth, height: barHeight)
+        
+        let additionLayout = ASOverlayLayoutSpec(child: toolsPanel, overlay: emotionPanel)
+        additionLayout.style.preferredSize = CGSize(width: Constants.screenWidth, height: panelHeight)
+        
+        let spacer = ASLayoutSpec()
+        spacer.style.preferredSize = CGSize(width: Constants.screenWidth, height: bottomInset)
+        
+        let stack = ASStackLayoutSpec.vertical()
+        stack.children = [toolBar, spacer, additionLayout]
+    
+        return ASInsetLayoutSpec(insets: .zero, child: stack)
     }
 }
