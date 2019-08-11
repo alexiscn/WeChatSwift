c3ac2b7571d6ff7e656ce6d50697607639f44dd1
alexiscn <xshf87@126.com>
2019-08-11 10:31:34 +0800

    Add MyFavoritesViewController

---
+++ WeChatSwift/Me/MyFavorites/MyFavoritesCellNode.swift
@@ -0,0 +1,9 @@
+//
+//  MyFavoritesCellNode.swift
+//  WeChatSwift
+//
+//  Created by xushuifeng on 2019/8/11.
+//  Copyright © 2019 alexiscn. All rights reserved.
+//
+
+import Foundation
---
+++ WeChatSwift/Me/MyFavorites/MyFavoritesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MyFavoritesViewController.swift
+//  WeChatSwift
+//
+//  Created by xushuifeng on 2019/8/11.
+//  Copyright © 2019 alexiscn. All rights reserved.
+//
+
+import Foundation
